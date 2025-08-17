a9abcb8239e37d6e0ce76d3a966ca11fe602cfd2
Poovarasu Sivanandame <poovarasu0603@gmail.com>
2025-08-17 23:07:16 +0530

    CRIME RECORD MANAGEMENT SYSTEM
    HI

---
+++ Crime Record Management System/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>T25 Police Station</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme.min.css">
+
+    <!-- Custom CSS -->
+    <link href="assets/css/simple-sidebar.css" rel="stylesheet">
+    <link href="assets/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    
+    <style>
+        body {
+            background-image: url('path/to/your/background-image.jpg');
+            background-size: cover; /* Cover the entire viewport */
+            background-position: center; /* Center the image */
+            background-repeat: no-repeat; /* Prevent the image from repeating */
+            height: 100vh; /* Full height */
+            margin: 0; /* Remove default margin */
+        }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse" style="background-color:darkslateblue;color: white; margin-bottom: 130px;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="admin/img/crime.png"class="rounded"></a>
+    </div>
+</nav>
+
+<!-- Your login form goes here -->
+
+</body>
+</html>
